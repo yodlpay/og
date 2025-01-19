e4e47fc241ca415145dba0def0dde9045e18d5d9
yodlmeister <sb@yodlpay.com>
2025-01-19 13:08:39 +0700

    less logs

--- src/lib/indexerClient.ts
+++ src/lib/indexerClient.ts
@@ -46,26 +46,15 @@ export async function fetchPaymentByTxHash(
   }
 
   const url = `${INDEXER_API_URL}/v1/payments/${txHash}`;
-  const options = { next: { revalidate: 7 * 24 * 3600 } };
-
-  console.log("fetching", url);
-
   const resp = await fetch(url);
 
-  console.log(resp);
-
   if (!resp.ok) {
-    console.log("resp", resp);
     if (resp.status == 404) {
-      console.log("Payment not found");
       throw new PaymentNotFoundError();
     }
     throw new Error("Failure");
   }
 
   const response = await resp.json();
-
-  console.log("response", response);
-
   return response as PaymentResponse;
 }
